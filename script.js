cb19129779f5db7eba0de49530586000f9b8ab2d
Jmaun22 <98067577+Jmaun22@users.noreply.github.com>
2022-03-29 18:49:41 -0400

    added lists to passgen function

--- script.js
+++ script.js
@@ -10,5 +10,32 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+  var pass = "dsafafas"
+  const specialCharsList = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "=", "+", "(", ")", "{", "}", "[", "]", "?", "/", ">", "<", ".", ",", "~", "|"];
+  const numberCharsList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+​
+  const uppercaseList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+​
+  const lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+  var length = prompt("The password must be at least 8 char long and no longer than 128 char.");
+  alert("The character types in the password can include lowercase, uppercase, numeric and special characters.")
+  var lowercase = prompt("Would you like lower case char type how many.");
+  var uppercase = prompt("Would you like upper case char, type how many you want.");
+  var numeric = prompt("Would you like numeric char, type how many you would like.")
+  var specialChar= prompt("Would you like special characters, type how many you want.")
+
+  var pass = 
+
+
+  return pass
+
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
